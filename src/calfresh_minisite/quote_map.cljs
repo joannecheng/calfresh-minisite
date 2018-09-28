570b606cdc45b08f354ec02c4bd757b0e060c757
Joanne Cheng <chengjoanne2@gmail.com>
2018-09-28 10:39:21 -0600

    Clean up HTML/css

--- src/calfresh_minisite/quote_map.cljs
+++ src/calfresh_minisite/quote_map.cljs
@@ -1,13 +1,11 @@
 (ns calfresh-minisite.quote-map
-  (:require-macros [cljs.core.async.macros :refer [go]]
-                   [clojure.string :as str])
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljsjs.mapbox]
             [cljsjs.ScrollMagic]
 
             [ajax.core :refer [GET POST]]
-            [clojure.string :as string]
 
-            [calfresh-minisite.quotes :as quotes]
+            [calfresh-minisite.quote-view :as quote-view]
             [calfresh-minisite.utils :as utils]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,13 +24,11 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Event Handling
-
 (defn scroll-action [element-id action controller]
   (let [el-height (utils/height-of element-id)]
     (-> (js/ScrollMagic.Scene.
          #js {:triggerElement (str "#" element-id) :duration (- el-height 80)})
         (.addTo controller)
-        ;;(.addIndicators)
         (.on "start end" action))))
 
 (defn scroll-handlers [quote-map]
@@ -42,50 +38,9 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Quotes Event Handlers
-
-;;TODO USE HICCUP GODDAMMIT THIS IS SO BAD
-(defn update-title [county-name]
-  (let [el (.getElementById js/document "quote_county_name")]
-    (set! (.-innerHTML el) (str "<h2>" county-name "</h2>"))))
-
-(defn county-info-row [county-data row-name item-name]
-  (let [item (get county-data item-name)]
-    (str "<div class=\"grid-item width-two-thirds\">"
-         row-name
-         "</div><div class=\"grid-item width-one-third end-row\">"
-         item
-         "</div>")))
-
-(defn county-info [county-name]
-  (let [county-data (get quotes/quotes county-name)]
-    (str "<div class=\"grid-box county-info\">"
-         (county-info-row county-data "Population" :population)
-         (county-info-row county-data "Min Cost of Living (2 Working Adults, 2 Children)"
-                          :minimum-cost-living-family)
-         (county-info-row county-data "Median Income" :median-income)
-         (county-info-row county-data "Poverty Rate" :poverty-rate)
-         "</div>")))
-
-(defn quote-html [county-quote]
-  (str "<blockquote class=\"animated fadeInDown\">"
-       county-quote
-       "</blockquote>"
-       ;;"<div class=\"profile-image\">Image here</div>"
-       ))
-
-(defn show-quotes [county-name]
-  (let [county-quotes (get (get quotes/quotes county-name) :quotes)
-        el (.getElementById js/document "quote_list")]
-    (set! (.-innerHTML el) "")
-
-    (if (> (count county-quotes) 0)
-      (set! (.-innerHTML el) (str (county-info county-name)
-                              (string/join " " (map quote-html county-quotes)))))))
-
-
 (defn update-quotes [county-name]
-  (update-title county-name)
-  (show-quotes county-name))
+  (quote-view/update-title county-name)
+  (quote-view/show-quotes county-name))
 
 (defn set-county-hover [container state-id hover-bool]
   (-> container
@@ -142,8 +97,7 @@
              (if (some? @hovered-state-id)
                (set-county-hover quote-map-container
                                  @hovered-state-id false))
-             (reset! hovered-state-id nil))))
-  )
+             (reset! hovered-state-id nil)))))
 
 (defn draw-map []
   (js/mapboxgl.Map.
@@ -151,8 +105,7 @@
         :style "mapbox://styles/mapbox/dark-v9",
         :maxBounds (js/mapboxgl.LngLatBounds.
                     #js [-124.48200988, 31.52952194]
-                    #js [-114.13077545, 43.00950241])
-        }))
+                    #js [-114.13077545, 43.00950241])}))
 
 (defn draw []
   (let [quote-map-container (draw-map)]
---
+++ src/calfresh_minisite/quote_view.cljs
@@ -0,0 +1,43 @@
+(ns calfresh-minisite.quote-view
+  (:require [clojure.string :as string]
+            [calfresh-minisite.quotes :as quotes]))
+
+;;TODO USE HICCUP GODDAMMIT THIS IS SO BAD
+(defn update-title [county-name]
+  (let [el (.getElementById js/document "quote_county_name")]
+    (set! (.-innerHTML el) (str "<h2>" county-name "</h2>"))))
+
+(defn county-info-row [county-data row-name item-name]
+  (let [item (get county-data item-name)]
+    (str "<div class=\"grid-item width-two-thirds\">"
+         row-name
+         "</div><div class=\"grid-item width-one-third end-row\">"
+         item
+         "</div>")))
+
+(defn county-info [county-name]
+  (let [county-data (get quotes/quotes county-name)]
+    (str "<div class=\"grid-box county-info\">"
+         (county-info-row county-data "Population" :population)
+         (county-info-row county-data "Min Cost of Living (2 Working Adults, 2 Children)"
+                          :minimum-cost-living-family)
+         (county-info-row county-data "Median Income" :median-income)
+         (county-info-row county-data "Poverty Rate (Cost of Living Adujusted)" :poverty-rate)
+         "</div>")))
+
+(defn quote-html [county-quote]
+  (str "<blockquote class=\"animated fadeInDown\">"
+       county-quote
+       "</blockquote>"
+       ;;"<div class=\"profile-image\">Image here</div>"
+       ))
+
+(defn show-quotes [county-name]
+  (let [county-quotes (get (get quotes/quotes county-name) :quotes)
+        el (.getElementById js/document "quote_list")]
+    (set! (.-innerHTML el) "")
+
+    (if (> (count county-quotes) 0)
+      (set! (.-innerHTML el) (str (county-info county-name)
+                                  (string/join " " (map quote-html county-quotes)))))))
+
