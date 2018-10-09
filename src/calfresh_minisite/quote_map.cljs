cc5ea806c301cff736f2f2e047321538acf47125
Joanne Cheng <chengjoanne2@gmail.com>
2018-10-09 12:26:10 -0600

    Add tooltips

--- src/calfresh_minisite/quote_map.cljs
+++ src/calfresh_minisite/quote_map.cljs
@@ -11,7 +11,7 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; State
-(def hovered-state-id (atom 38))
+(def selected-county-id (atom 38))
 (def selected-county-name (atom "Los Angeles"))
 (def map-animation (atom nil))
 
@@ -48,10 +48,11 @@
     (quote-view/update-title county-name)
     (quote-view/show-quotes county-name)))
 
-(defn set-county-hover [container state-id hover-bool]
+(defn set-selected-county [container county-id selected-bool]
+  (reset! selected-county-id county-id)
   (-> container
-      (.setFeatureState #js {:source "counties" :id state-id}
-                        #js {:hover hover-bool})))
+      (.setFeatureState #js {:source "counties" :id county-id}
+                        #js {:selected selected-bool})))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Drawing
@@ -60,40 +61,56 @@
   ;; See https://www.mapbox.com/mapbox-gl-js/example/hover-styles/ for more
   ;; information on what's going on
   [quote-map-container resp]
-  (-> quote-map-container
-      (.addSource "counties", (clj->js {:type "geojson" :data resp})))
-  (-> quote-map-container
-      (.addLayer (clj->js {:id "california-county-fill"
-                           :type "fill"
-                           :source "counties"
-                           :layout {}
-                           :paint {:fill-color "#e25050"
-                                   :fill-opacity ["case"
-                                                  ["boolean" ["feature-state" "hover"] false]
-                                                  0.6 0]}})))
+  (let [popup (js/mapboxgl.Popup. #js {:closeButton false :closeOnClick false})]
 
-  (set-county-hover quote-map-container
-                    @hovered-state-id true)
+    (-> quote-map-container
+        (.addSource "counties", (clj->js {:type "geojson" :data resp})))
+    (-> quote-map-container
+        (.addLayer (clj->js {:id "california-county-fill"
+                             :type "fill"
+                             :source "counties"
+                             :layout {}
+                             :paint {:fill-color "#e25050"
+                                     :fill-opacity ["case"
+                                                    ["boolean" ["feature-state" "selected"] false]
+                                                    0.6 0]}})))
+    (set-selected-county quote-map-container
+                         @selected-county-id true)
 
-  (-> quote-map-container
-      (.addLayer (clj->js {:id "california-counties"
-                           :type "line"
-                           :source "counties"
-                           :paint {:line-color "#ccc"
-                                   :line-width 1}})))
-  ;; TODO: TOOLTIPS FOR COUNTIES
+    ;; display tooltips
+    (-> quote-map-container
+        (.on "mousemove" "california-county-fill"
+             (fn [e]
+               (let [feature (first (.-features e))
+                     county-name (.-name (.-properties feature))
+                     html-str (str "<div>" county-name " County</div>")]
+                 (-> popup
+                     (.setHTML html-str)
+                     (.setLngLat (.-lngLat e))
+                     (.addTo quote-map-container))))))
 
-  (-> quote-map-container
-      (.on "click", "california-county-fill",
-           #(let [county-id (.-id (first (.-features %)))
-                  county-name (.-name (.-properties (first (.-features %))))]
-              (if (some? @hovered-state-id)
-                (set-county-hover quote-map-container
-                                  @hovered-state-id false))
-              (reset! selected-county-name county-name)
-              (set-county-hover quote-map-container county-id true)
-              (reset! hovered-state-id county-id)
-              (update-quotes)))))
+    (-> quote-map-container
+        (.on "mouseout" "california-counties"
+             (fn [] (.remove popup))))
+
+    (-> quote-map-container
+        (.addLayer (clj->js {:id "california-counties"
+                             :type "line"
+                             :source "counties"
+                             :paint {:line-color "#ccc"
+                                     :line-width 1}})))
+
+    (-> quote-map-container
+        (.on "click", "california-county-fill",
+             #(let [county-id (.-id (first (.-features %)))
+                    county-name (.-name (.-properties (first (.-features %))))]
+                (if (some? @selected-county-id)
+                  (set-selected-county quote-map-container
+                                       @selected-county-id false))
+                (reset! selected-county-name county-name)
+                (set-selected-county quote-map-container county-id true)
+                (update-quotes))))))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Scroll Event Handling
